18811b836db49e0b9418cb04dbfd772d7595395d
stvplus1 <semiroremari28@gmail.com>
2026-07-23 17:36:46 +0300

    Update MoviesActivity.java

--- app/src/main/MoviesActivity.java
+++ app/src/main/MoviesActivity.java
@@ -1,52 +1,69 @@
-package com.yourname.stvplus; // ناڤێ پاکێجا خۆ لێرە بنڤیسە
+package com.stvplus.player;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
+// ئەگەر کتێبخانەیێن ExoPlayer تە هەبن ل ڤێرە زێدە بکە وەکو د MainActivity دا هەنە
 
-public class MainActivity extends AppCompatActivity {
+public class MoviesActivity extends AppCompatActivity {
 
-    private WebView myWebView;
+    private WebView webView;
+    // ل ڤێرە گۆڕاوێن ExoPlayer یێن خۆ زێدە بکە (وەکو playerView, player)
 
-    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // دڤێت دیزاینا تە یا xml (activity_movies) هەم PlayerView و هەم WebView تێدا بن
+        setContentView(R.layout.activity_movies); 
+
+        // 1. ڕێکخستنا WebView
+        webView = findViewById(R.id.webView);
         
-        // شاردنەوەی شریتی سەرەوە بۆ ئەوەی شاشەکە پڕ بێت
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // 2. گرنگترین خاڵ: شەفافکرنا باکگراوەندێ WebView دا ڤیدیۆ ل پشتەڤە دیار بیت
+        webView.setBackgroundColor(0x00000000); 
 
-        myWebView = new WebView(this);
-        setContentView(myWebView);
-
-        WebSettings webSettings = myWebView.getSettings();
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
-        
-        // زۆر گرنگە بۆ تیڤی بۆکسێ
-        webSettings.setMediaPlaybackRequiresUserGesture(false); 
-        
-        webSettings.setAllowFileAccess(true);
-        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
 
-        myWebView.setWebViewClient(new WebViewClient());
+        // 3. دروستکرنا پردێ پەیوەندیێ دناڤبەرا HTML و جاڤا دا ب ناڤێ "AndroidPlayer"
+        webView.addJavascriptInterface(new WebAppInterface(), "AndroidPlayer");
 
-        // فایلێ خۆ یێ HTML لێرە ڤەکە
-        myWebView.loadUrl("file:///android_asset/index.html"); 
+        webView.setWebViewClient(new WebViewClient());
+        
+        // 4. لۆدکرنا فایلا فلیمان ژ ناو فۆلدەرێ assets (پشتڕاست بە ناڤێ فایلێ یێ دروستە)
+        webView.loadUrl("file:///android_asset/movies.html");
+
+        // 5. ل ڤێرە ExoPlayer یێ خۆ ئامادە بکە وەکو چەوا تە د MainActivity دا چێکریە
+        // setupExoPlayer(); 
     }
 
-    @Override
-    public void onBackPressed() {
-        if (myWebView.canGoBack()) {
-            myWebView.goBack();
-        } else {
-            super.onBackPressed();
+    // ئەڤ کلاسە فەرمانان ژ کۆدێ HTML وەردگریت
+    public class WebAppInterface {
+
+        @JavascriptInterface
+        public void playStream(String url, String type, String referer, String drmKeyId, String drmKey) {
+            runOnUiThread(() -> {
+                if (url != null && url.equals("about:blank")) {
+                    // ئەگەر فەرمانا "about:blank" هات، ئانکو بەکارهێنەری فلیم داخست، ڤیدیۆیێ ڕابگرە
+                    stopExoPlayer();
+                } else {
+                    // ل ڤێرە فەرمانێ بدە ExoPlayer دا ڤیدیۆیا نوی لێبدەت
+                    startExoPlayer(url, type);
+                }
+            });
         }
+    }
+
+    private void startExoPlayer(String url, String type) {
+        // کۆدێ کارپێکرنا ExoPlayer ل ڤێرە دابنێ (کۆپی بکە ژ MainActivity)
+    }
+
+    private void stopExoPlayer() {
+        // کۆدێ ڕاگرتنا ExoPlayer ل ڤێرە دابنێ
     }
 }
